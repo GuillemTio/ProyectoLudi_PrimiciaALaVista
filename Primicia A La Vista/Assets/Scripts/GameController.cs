f29d714f49d4c5c8c83cff42b042741f43202dc6
AdriaLopezGonzalez <alopezg@edu.tecnocampus.cat>
2023-11-10 14:59:24 +0100

    a

--- Primicia A La Vista/Assets/Scripts/GameController.cs
+++ Primicia A La Vista/Assets/Scripts/GameController.cs
@@ -7,6 +7,7 @@ public class GameController : MonoBehaviour
 {
     public List<Noticies> m_Noticies;
     public List<GameObject> m_NoticiaAugmentada;
+    int m_Index = 0;
 
     private void Update()
     {
@@ -14,11 +15,17 @@ public class GameController : MonoBehaviour
         {
             if (_Noticia.GetComponent<Noticies>().WordCompleted())
             {
+                m_Index = 0;
                 _Noticia.GetComponent<Button>().enabled = false;
             }
-
+            else if(!(m_Index > 0))
+            {
+                _Noticia.GetComponent<Button>().enabled = true;
+            }
+            
             if(_Noticia.GetComponent<Noticies>().m_Noticia.activeSelf == true)
             {
+                m_Index += 1;
                 foreach (Noticies _NoticiaButton in m_Noticies)
                 {
                     _NoticiaButton.GetComponent<Button>().enabled = false;
